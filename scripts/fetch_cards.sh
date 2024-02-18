9c05e44d45a6bb209c2a36e591aee9ab17658fc8
lplanch <github@lplanch.com>
2024-02-18 17:57:35 +0100

    -d path fixed in script

--- scripts/fetch_cards.sh
+++ scripts/fetch_cards.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if [ -d ${BABELCDB_PATH} ]; then
+if [ -d "${BABELCDB_PATH}" ]; then
     # if folder exists, fetch and check if there is changes on master
     echo "BabelCDB found, fetching new data..."
     git -C ${BABELCDB_PATH} checkout master
     git -C ${BABELCDB_PATH} fetch
-    if [[ $(git -C "${BABELCDB_PATH}" diff HEAD...origin/master) ]] || !([ -d ${DATABASE_PATH} ]); then
+    if [[ $(git -C "${BABELCDB_PATH}" diff HEAD...origin/master) ]] || !([ -d "${DATABASE_PATH}" ]); then
         # if there is changes or if DATABASE_URI file is not found, pull them and import the db
         git -C "${BABELCDB_PATH}" pull
         echo "Importing new card data..."
