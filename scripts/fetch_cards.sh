df250e2c34ab4c35e09f5bece1855d8cffcb1a9e
lplanch <github@lplanch.com>
2024-02-18 18:00:04 +0100

    script update added double quote to env var

--- scripts/fetch_cards.sh
+++ scripts/fetch_cards.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 if [ -d "${BABELCDB_PATH}" ]; then
     # if folder exists, fetch and check if there is changes on master
     echo "BabelCDB found, fetching new data..."
-    git -C ${BABELCDB_PATH} checkout master
-    git -C ${BABELCDB_PATH} fetch
+    git -C "${BABELCDB_PATH}" checkout master
+    git -C "${BABELCDB_PATH}" fetch
     if [[ $(git -C "${BABELCDB_PATH}" diff HEAD...origin/master) ]] || !([ -d "${DATABASE_PATH}" ]); then
         # if there is changes or if DATABASE_URI file is not found, pull them and import the db
         git -C "${BABELCDB_PATH}" pull
@@ -22,8 +22,8 @@ if [ -d "${BABELCDB_PATH}" ]; then
 else
     # folder not found, clone it
     echo "BabelCDB not found, cloning repository..."
-    git clone "https://github.com/ProjectIgnis/BabelCDB.git" ${BABELCDB_PATH}
-    git -C ${BABELCDB_PATH} checkout master
+    git clone "https://github.com/ProjectIgnis/BabelCDB.git" "${BABELCDB_PATH}"
+    git -C "${BABELCDB_PATH}" checkout master
     # upsert the database
     echo "Repository ready, importing card data..."
     go run ${SCRIPT_DIR}/setup_database.go
